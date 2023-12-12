441d75ceb7fe3635214323611cbeda62a3f21aca
binarywoo27 <blu_mountain@naver.com>
2023-12-12 15:27:52 +0900

    :recycle: 파일 위치 변경

--- src/main/java/kr/bb/order/dto/request/store/ProcessOrderDto.java
+++ src/main/java/kr/bb/order/dto/request/store/ProcessOrderDto.java
@@ -1,6 +1,5 @@
-package kr.bb.order.dto.kafka;
+package kr.bb.order.dto.request.store;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 import kr.bb.order.dto.request.orderForDelivery.OrderInfoByStore;
@@ -17,13 +16,13 @@ import lombok.NoArgsConstructor;
 public class ProcessOrderDto {
   private String orderGroupId;
   private List<Long> couponIds;
-  private List<Long> productIds;
+  private List<String> productIds;
 
   public static ProcessOrderDto toDto(String orderGroupId, List<OrderInfoByStore> orderInfoByStores){
 
     List<Long> couponIds = orderInfoByStores.stream().map(OrderInfoByStore::getCouponId).collect(
             Collectors.toList());
-    List<Long> productIds = orderInfoByStores.stream().flatMap(orderInfoByStore -> orderInfoByStore.getProducts().stream()).map(ProductCreate::getProductId).collect(
+    List<String> productIds = orderInfoByStores.stream().flatMap(orderInfoByStore -> orderInfoByStore.getProducts().stream()).map(ProductCreate::getProductId).collect(
             Collectors.toList());
 
     return ProcessOrderDto.builder()
