8cd0f76c8ae442cda906b9fd861e685da075cf1f
binarywoo27 <blu_mountain@naver.com>
2024-01-24 10:25:14 +0900

    refactor: 코드 정리

--- src/main/java/kr/bb/order/controller/clientcontroller/OrderClientController.java
+++ src/main/java/kr/bb/order/controller/clientcontroller/OrderClientController.java
@@ -1,10 +1,13 @@
 package kr.bb.order.controller.clientcontroller;
 
+import bloomingblooms.response.CommonResponse;
 import java.time.LocalDateTime;
 import kr.bb.order.entity.pickup.OrderPickupStatus;
+import kr.bb.order.service.OrderDetailsService;
 import kr.bb.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/client")
 @RequiredArgsConstructor
 public class OrderClientController {
-    private final OrderService orderService;
+  private final OrderService orderService;
+  private final OrderDetailsService orderDetailsService;
 
-    @GetMapping("/change")
-    public void updatePickupStatus() {
-        LocalDateTime now = LocalDateTime.now();
-        orderService.pickupStatusChange(now, OrderPickupStatus.COMPLETED);
-    }
+  @GetMapping("/change")
+  public void updatePickupStatus() {
+    LocalDateTime now = LocalDateTime.now();
+    orderService.pickupStatusChange(now, OrderPickupStatus.COMPLETED);
+  }
+
+  @GetMapping("/{orderId}/delivery-id")
+  CommonResponse<Long> getDeliveryId(@PathVariable String orderId) {
+    return CommonResponse.success(orderDetailsService.getDeliveryId(orderId));
+  }
 }
